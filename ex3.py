911bc7dc6f1db3bcfa457f023a2c0840baa525cd
Nitin Aggarwal <nitin.leo1986@gmail.com>
2015-09-08 11:43:49 +0530

    Exercises from Zed Shaw on Learn Python the Hard Way.

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# It will print the comment: I will now count my chickens:
+print "I will now count my chickens:"
+
+# prints Hens, and then result of 25 + 30 / 6, and then in next line
+# it prints Roosters, and then result of 100 - 25 * 3 % 4.
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints: Now I will count the eggs:
+print "Now I will count the eggs:"
+
+# prints the result as true or false for 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6.0
+
+# prints: Is it true that 3 + 2 < 5 - 7?
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# prints the result as true or false for of 3 + 2 < 5 - 7
+print 3 + 2 < 5 - 7
+
+# prints: What is 3 + 2? and then the result of 3 + 2
+# prints: What is 5 - 7? and then the result of 5 - 7
+print "What is 3 + 2?", 3 + 2 
+print "What is 5 - 7?", 5 - 7
+
+# prints: Oh, that's why it's False.
+print "Oh, that's why it's False."
+
+# prints: How about some more. 
+print "How about some more."
+
+# prints: Is it greater? and then the result of 5 > -2
+# prints: Is it greater or equal? and then result of 5 >= -2
+# prints: Is it less or equal? and then result of 5 <= -2
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
