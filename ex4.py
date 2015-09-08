8d8fc2613e25e51b44c9ed2f948ab1b95311f6aa
Nitin Aggarwal <nitin.leo1986@gmail.com>
2015-09-08 15:23:38 +0530

    Updated the exercise 4 with necessary changes.

--- ex4.py
+++ ex4.py
@@ -1,10 +1,19 @@
+# Defined cars as a variable with value 100.
 cars = 100
+# Defined space_in_a_car as a variable with value 4.0.
 space_in_a_car = 4.0
+# Defined drivers as a variable with value 30.
 drivers = 30
+# Defined passengers as a variable with value 90.
 passengers = 90
+# Defined cars_not_driven as a variable with its value as difference of 
+# cars and drivers.
 cars_not_driven = cars - drivers
+# Defined cars_driven as a variable with value equal to drivers.
 cars_driven = drivers
+# Defined carpool_capacity as the product of cars_driven times the space_in_a_car.
 carpool_capacity = cars_driven * space_in_a_car
+# Defined the average_passengers_per_car as the ratio of passengers to cars_driven.
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
