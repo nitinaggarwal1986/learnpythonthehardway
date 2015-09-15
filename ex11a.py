e9d33b3156f94e5cc31c5e1befa3498952d07224
Nitin Aggarwal <nitin.leo1986@gmail.com>
2015-09-15 12:02:06 +0530

    Finished the exercise 11 and an addition to it for practice.

---
+++ ex11a.py
@@ -0,0 +1,9 @@
+print "Are you bored? (Yes/No)",
+Answer = raw_input()
+
+if Answer == "Yes":
+	print "You should do something."
+elif Answer == "No":
+	print "I am glad that you are keeping yourself busy."
+else:
+	print "I need you to reply properly, i.e. 'Yes' if you are bored and 'No' if you are not."
