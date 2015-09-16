f38a22add0d31354bdf57de4ab3fd0db2f6d9c90
Nitin Aggarwal <nitin.leo1986@gmail.com>
2015-09-16 11:50:53 +0530

    Uploading the exercises 13 and 14.

---
+++ ex13c.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third, fourth = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "You have an extra variable, the fourth variable:", fourth
+print "You can enter something extra, say the fifth variable:",
+fifth = raw_input()
+print "Your fifth variable is now:", fifth
